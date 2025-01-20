fc9d8e07253c8bef04039fa9675859ece950d7a6
rainezaldivar <zaldivarraine17@gmail.com>
2025-01-20 10:41:27 +0800

    01/20/25 1041 - First  Python Programming Activity

--- act1.py
+++ act1.py
@@ -1,2 +1,36 @@
-name = "Raine"
-print("My name is:", name)
+fname = input("Enter your First Name: ")
+print("First Name:", fname)
+
+lname = input("Enter your Last Name: ")
+print("Last Name:", lname)
+
+midname = input("Enter your Middle Name: ")
+print("Middle Name:", midname)
+
+bday = input("When is your Birthday? ")
+print("Birthday:", bday)
+
+gender = input("Your Gender: ")
+print("Gender:", gender)
+
+home_add = input("What is your Home Address? ")
+print("Address:", home_add)
+
+course = input("Your course currently taking: ")
+print("Course:", course)
+
+yrlvl = input("What Year Level are you now?")
+print("Year Level:", yrlvl)
+
+print("=================================================")
+print("                  PERSONAL INFO                  ")
+print("=================================================")
+print("     ")
+print("First Name:", fname)
+print("Last Name:", lname)
+print("Middle Name:", midname)
+print("Birthday:", bday)
+print("Gender:", gender)
+print("Address:", home_add)
+print("Course:", course)
+print("Year Level:", yrlvl)
